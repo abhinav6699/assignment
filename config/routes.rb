bd8bbf422adea9cf77777e3c31abf2b9710c96fc
Kumar Abhinav <kmrabhinav100@gmail.com>
2022-01-26 18:28:22 +0530

    Add files via upload

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root   'home#index'
+  get    'edit'    => 'home#edit'
+  get    'signup'  => 'users#create'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
+  resources :profiles , only: [:update]
+  resources :educations , only: [:new]
+end
